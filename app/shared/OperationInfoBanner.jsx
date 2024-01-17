c5309b778286977907ea788e6fee38e5def47469
butadpj <butadpj@gmail.com>
2024-01-17 11:44:16 +0800

    Marquee

--- app/shared/OperationInfoBanner.jsx
+++ app/shared/OperationInfoBanner.jsx
@@ -3,9 +3,9 @@ export default function OperationInfoBanner({ className = "" }) {
     <div
       className={`fixed top-0 z-40 w-full bg-orange-600 text-white ${className}`}
     >
-      <h2 className="text-center">
+      <marquee className="text-center text-lg" scrollamount="10">
         !! Orders accepted are limited to Calumpang, Marikina branches only !!
-      </h2>
+      </marquee>
     </div>
   );
 }
