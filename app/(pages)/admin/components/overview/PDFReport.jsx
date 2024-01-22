1012e100fb0e4dea694f4ff8299f137e0f8d6b09
Josh Danielle Domingo <102298089+Shijel@users.noreply.github.com>
2024-01-22 15:15:59 +0800

    Update PDFReport.jsx

--- app/(pages)/admin/components/overview/PDFReport.jsx
+++ app/(pages)/admin/components/overview/PDFReport.jsx
@@ -65,7 +65,7 @@ const PDFreport = async (reportData, startDate, endDate) => {
   document.setFontSize(10);
   document.text("Sales Report", marginLeft, 70);
   document.text("Record of Sales: ", marginLeft, 100);
-  document.text("Date Generated: ");
+  document.text("Date Generated: ",marginLeft, 100);
   console.log(dateGenerated);
   
   autoTable(document, content);
